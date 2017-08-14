01bbb12efa98fe7375f91c1ad00733ec1ad9a716
Dusan Vorkapic <duskox@gmail.com>
2017-08-14 16:19:24 +0200

    First commit

---
+++ app/src/main/java/com/futurice/dvor/secretsanta/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.futurice.dvor.secretsanta
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.hello.setText("BLA")
+
+    }
+}
